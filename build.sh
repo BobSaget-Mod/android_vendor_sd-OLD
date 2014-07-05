3844bbb85829b3c9ecac05092ff9ee67eab45e4a
Michael S Corigliano (MikeCriggs@XDA) <michael.s.corigliano@gmail.com>
2014-07-05 12:06:01 -0400

    build.sh: edit for AOSPSM

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 #
 # Created by Michael S Corigliano for Team AOSPAL (michael.s.corigliano@gmail.com)
+#
 # Parts of the original AOSPA build script have also been implemented in this script,
 # it can be found here: https://www.github.com/AOSPA/android_vendor_pa/build.sh
 #
@@ -18,42 +19,17 @@
 # limitations under the License.
 #
 
-# Get build version
-#   PA_MAJOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAJOR := *' | sed  's/ROM_VERSION_MAJOR := //g')
-#   PA_MINOR=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MINOR := *' | sed  's/ROM_VERSION_MINOR := //g')
-#   PA_MAINTENANCE=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_MAINTENANCE := *' | sed  's/ROM_VERSION_MAINTENANCE := //g')
-#   PA_TAG=$(cat $DIR/vendor/pa/vendor.mk | grep 'ROM_VERSION_TAG := *' | sed  's/ROM_VERSION_TAG := //g')
-#
-#   PSD_MAJOR=$(cat $DIR/vendor/psd/vendor.mk | grep 'PSD_VERSION_MAJOR := *' | sed  's/PSD_VERSION_MAJOR := //g')
-#   PSD_MINOR=$(cat $DIR/vendor/psd/vendor.mk | grep 'PSD_VERSION_MINOR := *' | sed  's/PSD_VERSION_MINOR := //g')
-#   PSD_MAINTENANCE=$(cat $DIR/vendor/psd/vendor.mk | grep 'PSD_VERSION_MAINTENANCE := *' | sed  's/PSD_VERSION_MAINTENANCE := //g')
-#   PSD_TAG=$(cat $DIR/vendor/psd/vendor.mk | grep 'PSD_TYPE := *' | sed  's/PSD_TYPE := //g')
-#
-#   if [ -n "$PA_TAG" ]; then
-#      VERSION=$MAJOR.$MINOR$MAINTENANCE-$TAG
-#   else
-#      VERSION=$MAJOR.$MINOR$MAINTENANCE
-#   fi
-#
-#   if [ -n "$PSD_TAG" ]; then
-#      PSD_VERSION=$PSD_MAINTENANCE-$PSD_TAG-$PSD_MAJOR.$PSD_MINOR
-#   else
-#      PSD_VERSION=$PSD_MAINTENANCE-$PSD_MAJOR.$PSD_MINOR
-#   fi
-
 DEVICE="$1"
-timestamp="$(date +%s)"
  
 # start
-   echo -e "Building Paranoid SaberDroid for $DEVICE";
-#   echo -e "$PA_TAG $PA_MAJOR.$PA_MINOR";
-#   echo -e "$PSD_TAG $PSD_MAJOR.$PSD_MINOR for $DEVICE";
+   echo -e "Building AOSPAL 'AOSPSM' for $DEVICE";
    echo -e "";
    echo -e ""
  
 # make 'build-logs' directory if it doesn't already exist
    echo -e "Making a 'build-logs' directory if you haven't already...";
    mkdir -p build-logs
+   clear
  
 # fetch latest sources
    echo -e "Fetching latest sources...";
@@ -108,9 +84,9 @@ timestamp="$(date +%s)"
          2) deodex"
       read n
          case $n in
-            1) lunch psd_$DEVICE-user
+            1) lunch aospal_$DEVICE-user
                ;;
-            2) lunch psd_$DEVICE-userdebug
+            2) lunch aospal_$DEVICE-userdebug
                ;;
             *) invalid option
                ;;
@@ -126,20 +102,20 @@ timestamp="$(date +%s)"
          4) -j32"
       read n
          case $n in
-            1) make -j4 bacon 2>&1 | tee build-logs/psd_$DEVICE-$timestamp.txt
+            1) make -j4 bacon 2>&1 | tee build-logs/aospal_$DEVICE.txt
                ;;
-            2) make -j8 bacon 2>&1 | tee build-logs/psd_$DEVICE-$timestamp.txt
+            2) make -j8 bacon 2>&1 | tee build-logs/aospal_$DEVICE.txt
                ;;
-            3) make -j18 bacon 2>&1 | tee build-logs/psd_$DEVICE-$timestamp.txt
+            3) make -j18 bacon 2>&1 | tee build-logs/aospal_$DEVICE.txt
                ;;
-            4) make -j32 bacon 2>&1 | tee build-logs/psd_$DEVICE-$timestamp.txt
+            4) make -j32 bacon 2>&1 | tee build-logs/aospal_$DEVICE.txt
                ;;
             *) invalid option
                ;;
          esac
  
 # we're done
-   echo -e "Finished building Paranoid SaberDroid.";
+   echo -e "Finished building.";
    echo -e "If for some reason your build failed,";
    echo -e "please check the 'build-logs' directory to figure out why.";
    echo -e "";
