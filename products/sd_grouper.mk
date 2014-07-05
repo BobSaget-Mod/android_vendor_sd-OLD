74f32b5be8d85c7fb1783b6d1f55be1c89adad26
Michael S Corigliano (MikeCriggs@XDA) <michael.s.corigliano@gmail.com>
2014-07-05 13:38:55 -0400

    sd_grouper.mk: SaberDroidify!

--- products/sd_grouper.mk
+++ products/sd_grouper.mk
@@ -1,6 +1,6 @@
-ifeq (aospal_grouper,$(TARGET_PRODUCT))
+ifeq (sd_grouper,$(TARGET_PRODUCT))
 
-include vendor/aospal/configs/aospal_modular.mk
+include vendor/sd/configs/sd_modular.mk
 
 # Set -fstrict-aliasing flag to global for flo
 MAKE_STRICT_GLOBAL := true
@@ -11,17 +11,17 @@ ENABLE_GRAPHITE := true
 # Saber linux toolchains
 USING_SABER_LINUX := yes
 
-# Include AOSPAL common configuration
-include vendor/aospal/main.mk
+# Include SaberDroid common configuration
+include vendor/sd/main.mk
 
-# Call AOSPAL device
-$(call inherit-product, vendor/aospal/products/aospal_grouper.mk)
+# Call SaberDroid device
+$(call inherit-product, vendor/sd/products/sd_grouper.mk)
 
 # Inherit AOSP device configuration
 $(call inherit-product, device/asus/grouper/full_grouper.mk)
 
 # Override AOSP build properties
-PRODUCT_NAME := aospal_grouper
+PRODUCT_NAME := sd_grouper
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
