6b413ac16d01e131d2dc2cf13bf3a882dba981fc
Paul Beeler <pbeeler80@gmail.com>
2014-09-09 09:25:20 -0600

    4.10 magically turned to 5.0, big thanks to GNU
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- configs/sd_board.mk
+++ configs/sd_board.mk
@@ -31,7 +31,7 @@ SM_AND := $(shell $(SM_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
 ifneq ($(filter (SM-Toolchain) (SaberMod%),$(SM_AND)),)
 # GCC Colors only works on gcc >=4.9.x
 $(shell `export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'`)
-SM_AND_VERSION := $(filter 4.8 4.8.% 4.9 4.9.% 4.10 4.10.%,$(SM_AND))
+SM_AND_VERSION := $(filter 4.8 4.8.% 4.9 4.9.% 5.0 5.0.%,$(SM_AND))
 SM_AND_NAME := $(filter (SM-Toolchain) (SaberMod%),$(SM_AND))
 SM_AND_DATE := $(filter 20130% 20131% 20140% 20141%,$(SM_AND))
 SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
@@ -58,7 +58,7 @@ SM_ARM := $(shell $(SM_ARM_PATH)/bin/arm-eabi-gcc --version)
 ifneq ($(filter (SM-Toolchain) (SaberMod%),$(SM_ARM)),)
 # GCC Colors only works on gcc >=4.9.x
 export GCC_COLORS := 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-SM_ARM_VERSION := $(filter 4.8 4.8.% 4.9 4.9.% 4.10 4.10.%,$(SM_ARM))
+SM_ARM_VERSION := $(filter 4.8 4.8.% 4.9 4.9.% 5.0 5.0.%,$(SM_ARM))
 SM_ARM_NAME := $(filter (SM-Toolchain) (SaberMod%),$(SM_ARM))
 SM_ARM_DATE := $(filter 20130% 20131% 20140% 20141%,$(SM_ARM))
 SM_ARM_STATUS := $(filter (release) (prerelease) (experimental),$(SM_ARM))
